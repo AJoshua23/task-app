a833b12aff97f21e0065b8bb3f65ab8c2e59152d
AJoshua23 <toni.nikolaev.23@gmail.com>
2019-12-12 17:03:46 +0200

    Third commit

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+       // const tasks = await Task.find({owner: req.user._id})
+       await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send(e)
     }
@@ -14,11 +16,12 @@ router.get('/tasks', async (req, res) => {
 
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        //const task = await Task.findById(_id)
+        const task = await Task.findOne({_id, owner: req.user._id})
         if(!task){
             return res.status(404).send()
         }
@@ -67,8 +70,11 @@ router.delete('/tasks/:id', async(req, res) => {
     }
 })
 
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
 
     try {
         await task.save()
